e16086d105b3558aec560f37d3045aab4869886d
juliog75 <120219346+juliog75@users.noreply.github.com>
2023-02-28 21:57:51 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const who = ['El perro', 'Mi abuelo', 'Su tortuga', 'Mi pajaro'];
+const what = ['se ha comido', 'ha molestado', 'ha aplastado', 'se ha roto'];
+const when = [
+  'antes de la clase',
+  'justo a tiempo',
+  'justo a tiempo',
+  'durante mi almuerzo',
+  'mientras estaba rezando'
+];
+
+function generadorExcusa() {
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+  return `${randomWho} ${randomWhat} ${randomWhen}`;
+}
+
+window.onload = function() {
+    document.getElementById('excuse').innerHTML = generadorExcusa();
+};
+  
